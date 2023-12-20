80865a5c90911ae7d0c5b536709a1621299f68b8
Konstantin <reverant_01@mail.ru>
2023-12-20 18:43:35 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
+
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
